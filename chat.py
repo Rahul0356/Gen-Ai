5bb5b4d8cec697e0e8421f7b514fca1c23ecd0c9
Rahul Kumar <rahulkumar9199270356@gmail.com>
2026-03-05 16:47:10 +0530

    prompting and embedding understing

---
+++ chat.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+from openai  import OpenAI
+
+load_dotenv()
+
+client = OpenAI()
+
+result = client.chat.completions.create(
+    model="gpt-4",
+    messages=[
+        {"role": "user", "content": "What is the capital of France?"},
+    ]
+)
+print(result.choices[0].message.content)
---
+++ tokenization.py
@@ -0,0 +1,9 @@
+import tiktoken
+
+encoder = tiktoken.encoding_for_model('gpt-4o')
+print('Vocab Size ',encoder.n_vocab)
+
+text="The cat sat on the mat."
+tokens = encoder.encode(text)
+
+print("Tokens: ", tokens)
